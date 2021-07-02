e8c0507be020285bf5ba5b70b59fd5a6b8fa99c1
Amit Baid <baidstar@hotmail.com>
2021-07-01 23:29:48 -0600

    lessons on objects and arrays

---
+++ judgeVegtable.js
@@ -0,0 +1,33 @@
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+const judgeVegetable = function (vegetables, metric) {
+  let top = 0
+  let winner = ""
+  for (i = 0; i < vegetables.length; i++) {
+    if(vegetables[i][metric] > top) {
+      top = vegetables[i][metric]
+      winner = i
+    }
+  }
+  return vegetables[winner].submitter
+} 
+
+console.log(judgeVegetable(vegetables, metric))
---
+++ smartGarbage.js
@@ -0,0 +1,9 @@
+const smartGarbage = function(trash, bins) {
+ if(bins[trash]) {
+   bins[trash] += 1
+ }
+  return bins
+}
+
+
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
